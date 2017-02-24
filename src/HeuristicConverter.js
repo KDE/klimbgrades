646960fe376363f81f9339fea7f0a796839bc905
Marco Martin <notmart@gmail.com>
2017-02-24 16:20:28 +0100

    add a simple conversion to roman numerals for uiaa

--- src/HeuristicConverter.js
+++ src/HeuristicConverter.js
@@ -68,6 +68,23 @@ function formatFrench(decimalGrade)
     return Math.floor(adjusted) + letter + plus;
 }
 
+function convertToRoman(num) {
+
+    var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+    var romanized = '';
+
+    for (var index = 0; index < decimalValue.length; index++) {
+        while (decimalValue[index] <= num) {
+            romanized += romanNumeral[index];
+            num -= decimalValue[index];
+        }
+    }
+
+    return romanized;
+}
+
 function formatUIAA(decimalGrade)
 {
     var adjusted = decimalGrade/3 - 22 + 1/3;
@@ -82,7 +99,7 @@ function formatUIAA(decimalGrade)
     } else {
         sign = "+";
     }
-    return Math.round(adjusted) + sign;
+    return convertToRoman(Math.round(adjusted)) + sign;
 }
 
 function formatFont(decimalGrade)
